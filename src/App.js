a333984fa04f3266afd2de7972a020202b95aec7
pekobeni <arimkim.workspace@gmail.com>
2024-07-02 15:50:32 +0900

    front react upload 1

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Router from "./routes/indexRouter";
+
+function App() {
+  return <Router />;
+}
+
+export default App;
---
+++ src/pages/member/MemberIndex.js
@@ -0,0 +1,16 @@
+import BasicLayout from "../../layouts/BasicLayout";
+import { Outlet } from "react-router-dom";
+
+const MemberIndex = () => {
+  return (
+    <BasicLayout>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </BasicLayout>
+  );
+};
+
+export default MemberIndex;
